d83aec47d1e21ff371e15a29064cb0ce93046147
Rolland Yann <rollandyann@gmail.com>
2016-03-26 17:37:50 +0100

    Contact

--- app/inc/contact.php
+++ app/inc/contact.php
@@ -1,7 +1,24 @@
-<section class="contact">
+<article class="contact">
 	
 	<h2>Nous contacter</h2>
-	<a href="mailto:hello@fluxi.digital" title="Nous contacter">hello@fluxi.digital</a>
+
+	<ul class="contact-equipe">
+		<li>
+			<h4 class="contact-equipe__nom">Julien Pecorino</h4>
+			<p class="contact-equipe__job">UX Design, Front-end</p>
+		</li>		
+		<li>
+			<h4 class="contact-equipe__nom">Thibaut Caroli</h4>
+			<p class="contact-equipe__job">UX Design, Front-end</p>
+		</li>
+		<li>
+			<h4 class="contact-equipe__nom">Yann Rolland</h4>
+			<p class="contact-equipe__job">Développement, Back-end</p>
+		</li>
+	</ul>
+	
+	<h3>Une question à poser ?</h3>
+	<a class="contact-mail" href="mailto:hello@fluxi.digital" title="Nous contacter">hello@fluxi.digital</a>
 	
 	
-</section>
+</article>
