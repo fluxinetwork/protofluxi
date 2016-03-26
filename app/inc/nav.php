64e58f3276daada811445381f23a22bd3fc3126d
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2016-03-26 17:47:42 +0100

    JS scroll

--- app/inc/nav.php
+++ app/inc/nav.php
@@ -1,1 +1,8 @@
-<nav class="class"></nav>
+<nav class="nav">
+	<ul class="navlist">
+		<li class="navlist__item js-scroll-to">Intro</li>
+		<li class="navlist__item js-scroll-to">Compétences</li>
+		<li class="navlist__item js-scroll-to">Methode UX</li>
+		<li class="navlist__item js-scroll-to">Contact</li>
+	</ul>
+</nav>
