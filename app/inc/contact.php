db5c8cc226712659d4bdf27ba8086875675493bd
Rolland Yann <rollandyann@gmail.com>
2016-03-26 17:19:26 +0100

    Ux

---
+++ app/inc/contact.php
@@ -0,0 +1,7 @@
+<section class="contact">
+	
+	<h2>Nous contacter</h2>
+	<a href="mailto:hello@fluxi.digital" title="Nous contacter">hello@fluxi.digital</a>
+	
+	
+</section>
---
+++ app/inc/uxdesign.php
@@ -0,0 +1,15 @@
+<section class="uxdesign">
+	
+	<h2>Identifier les objectifs et les utilisateurs</h2>
+	<p> Maecenas condimentum pretium lectus porta tincidunt. Vivamus lorem massa, dignissim venenatis laoreet id, placerat id purus. Fusce tempor nulla eu efficitur porta. Vivamus eu mi non dolor dictum placerat. Integer et ligula vitae velit venenatis elementum vitae ac tellus. Nunc eget lectus nibh. Nam eget commodo sapien. Donec finibus dictum dolor ut egestas. </p>
+	
+	<h2>Valider des hypothèses fonctionnelles</h2>
+	<p> Maecenas condimentum pretium lectus porta tincidunt. Vivamus lorem massa, dignissim venenatis laoreet id, placerat id purus. Fusce tempor nulla eu efficitur porta. Vivamus eu mi non dolor dictum placerat. Integer et ligula vitae velit venenatis elementum vitae ac tellus. Nunc eget lectus nibh. Nam eget commodo sapien. Donec finibus dictum dolor ut egestas. </p>
+	
+	<h2>Analyser les retours utilisateurs</h2>
+	<p> Maecenas condimentum pretium lectus porta tincidunt. Vivamus lorem massa, dignissim venenatis laoreet id, placerat id purus. Fusce tempor nulla eu efficitur porta. Vivamus eu mi non dolor dictum placerat. Integer et ligula vitae velit venenatis elementum vitae ac tellus. Nunc eget lectus nibh. Nam eget commodo sapien. Donec finibus dictum dolor ut egestas. </p>
+	
+	<h2>Améliorer le produit</h2>
+	<p> Maecenas condimentum pretium lectus porta tincidunt. Vivamus lorem massa, dignissim venenatis laoreet id, placerat id purus. Fusce tempor nulla eu efficitur porta. Vivamus eu mi non dolor dictum placerat. Integer et ligula vitae velit venenatis elementum vitae ac tellus. Nunc eget lectus nibh. Nam eget commodo sapien. Donec finibus dictum dolor ut egestas. </p>
+	
+</section>
