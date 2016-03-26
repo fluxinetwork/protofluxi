a895e921e24b4f57e231469a70b9d69347d7654a
Thibaut Caroli <thibaut.caroli.pro@gmail.com>
2016-03-26 17:17:43 +0100

    Skills

---
+++ app/inc/skills.php
@@ -0,0 +1,40 @@
+<section class="skills">
+	<header class="section-header">
+		<h4>Proin arcu purus, congue ac dapibus non, imperdiet eu velit. In mattis nunc risus, congue rutrum justo egestas eget. Curabitur a justo sed sapien dictum aliquam. Donec laoreet pulvinar nisl sed dapibus. Pellentesque id magna convallis, pharetra tortor sed, mollis nulla.</h4>
+	</header>
+	
+	<ul class="skills-categories">
+		<li class="skills-category platform">
+			<p>Description categorie et bullshit nigga DMLA</p>
+			<ul class="skills-list">
+				<li class="skills-list__item">Sites web & Responsive design</li>
+				<li class="skills-list__item">Application mobile</li>
+				<li class="skills-list__item">Logiciel</li>
+			</ul>
+		</li>
+
+		<li class="skills-category content">
+			<p>Description categorie et bullshit nigga DMLA</p>
+			<ul class="skills-list">
+				<li class="skills-list__item">Stratégie de contenu</li>
+			</ul>
+		</li>
+
+		<li class="skills-category visual">
+			<p>Description categorie et bullshit nigga DMLA</p>
+			<ul class="skills-list">
+				<li class="skills-list__item">Design d’interface</li>
+				<li class="skills-list__item">Identité visuelle pour le digital</li>
+			</ul>
+		</li>
+
+		<li class="skills-category technology">
+			<p>Description categorie et bullshit nigga DMLA</p>
+			<ul class="skills-list">
+				<li class="skills-list__item">Solutions techniques</li>
+				<li class="skills-list__item">Gestion de contenu intelligente</li>
+				<li class="skills-list__item">Mesures d’efficacité</li>
+			</ul>
+		</li>
+	</ul>
+</section>
